87048c05ca49b6933d0db719541f710e0a2a4a31
xiaoxunyao <huchengyea@163.com>
2022-05-04 15:16:39 +0800

    chore: init `test` model

---
+++ test/core-test/src/isTrue.test.ts
@@ -0,0 +1,8 @@
+import { test } from 'vitest'
+import {isTrue} from '@unboxing/core'
+
+test('test is true',()=>{
+  console.log(isTrue(true))
+  console.log(isTrue('true'))
+  console.log(isTrue('false'))
+})
