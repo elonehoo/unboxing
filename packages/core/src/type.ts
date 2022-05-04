1d5ec3444b942964add73a550504dc8605955351
xiaoxunyao <huchengyea@163.com>
2022-05-04 15:09:55 +0800

    chore: init type

---
+++ packages/core/src/type.ts
@@ -0,0 +1,17 @@
+/**
+ * Gives a single-word string description of the (native) type of a value,
+ * returning such answers as 'Object', 'Number', 'Array', or 'Null'. Does not
+ * attempt to distinguish user Object types any further, reporting them all as
+ * 'Object'.
+ */
+export function typeOf(x: any): string {
+  if (x === null) {
+    return 'Null';
+  }
+
+  if (x === undefined) {
+    return 'Undefined';
+  }
+
+  return Object.prototype.toString.call(x).slice(8, -1);
+}
