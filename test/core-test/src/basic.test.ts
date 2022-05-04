821905ebed04d0ff01a68314e01be4d0d049d8ad
xiaoxunyao <huchengyea@163.com>
2022-05-04 15:10:04 +0800

    test: init model

--- test/core-test/src/basic.test.ts
+++ test/core-test/src/basic.test.ts
@@ -1,1 +1,12 @@
 import { test } from 'vitest'
+import {typeOf} from '@unboxing/core'
+
+test('test type of',()=>{
+  console.log(typeOf({})) // -> 'Object'
+  console.log(typeOf(1)) // -> 'Number'
+  console.log(typeOf(false)) // -> 'Boolean'
+  console.log(typeOf('string')) // -> 'String'
+  console.log(typeOf(null)) // -> 'Null'
+  console.log(typeOf([])) // -> 'Array'
+  console.log(typeOf(/[A-z]/)) // -> 'RegExp'
+})
