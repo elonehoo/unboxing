2fe7a107ff045b6c92e81cca418f795f549841af
xiaoxunyao <huchengyea@163.com>
2022-05-04 18:01:13 +0800

    chore: init `not` test model

---
+++ test/core-test/src/not.test.ts
@@ -0,0 +1,17 @@
+import { test } from 'vitest'
+import {not} from '@unboxing/core'
+
+test('test not',()=>{
+  console.log(not(true)) // -> false
+  console.log(not(false)) // -> true
+  console.log(not(0)) // -> true
+  console.log(not(1)) // -> false
+  console.log(not('')) // -> true
+  console.log(not(' ')) // -> false
+  console.log(not('0')) // -> false
+  console.log(not('1')) // -> false
+  console.log(not([])) // -> false
+  console.log(not([1])) // -> false
+  console.log(not({})) // -> false
+  console.log(not({a:1})) // -> false
+})
