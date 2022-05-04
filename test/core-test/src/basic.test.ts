f5ad5788a966980b3351306554726e8cc60c0555
xiaoxunyao <huchengyea@163.com>
2022-05-04 14:47:42 +0800

    fix: update

--- test/core-test/src/basic.test.ts
+++ test/core-test/src/basic.test.ts
@@ -1,15 +1,1 @@
 import { test } from 'vitest'
-
-import {root,ArrayProto,ObjProto} from "@unboxing/core"
-
-test('test root type',()=>{
-  console.log(typeof root)
-})
-
-test('test Array Proto',()=>{
-  console.log(ArrayProto.push)
-})
-
-test('test Object Proto',()=>{
-  console.log(ObjProto)
-})
