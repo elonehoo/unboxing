21076b00bf69a4758a31884e3e48362e84f2920e
xiaoxunyao <huchengyea@163.com>
2022-05-04 19:53:58 +0800

    fix: update

--- test/function-test/src/alwats.test.ts
+++ test/function-test/src/alwats.test.ts
@@ -1,7 +1,7 @@
 import {test} from 'vitest'
 import { alaways } from '@unboxing/function'
 
-test('test alaways',()=>{
+test.skip('test alaways',()=>{
   let f = alaways('Foo')
   console.log(f()) // -> 'Foo'
 })
