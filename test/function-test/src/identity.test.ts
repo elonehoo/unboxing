73bebd42816116450e5faeeb79aa4650d72024f5
xiaoxunyao <huchengyea@163.com>
2022-05-06 09:58:15 +0800

    chore: init `identity` test

---
+++ test/function-test/src/identity.test.ts
@@ -0,0 +1,8 @@
+import { test } from "vitest"
+import { identity } from "@unboxing/function"
+
+test('test identity',()=>{
+  console.log(identity(1)) // -> 1
+  console.log(identity(true)) // -> true
+  console.log(identity(false)) // -> false
+})
