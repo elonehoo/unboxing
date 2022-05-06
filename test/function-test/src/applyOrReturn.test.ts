1bf9d95b7d7583d61e6d0448b36d3ba7b5df9eae
xiaoxunyao <huchengyea@163.com>
2022-05-06 10:41:00 +0800

    chore: init `appleOrReturn` test

---
+++ test/function-test/src/applyOrReturn.test.ts
@@ -0,0 +1,7 @@
+import { applyOrReturn } from "@unboxing/function";
+import { test } from "vitest";
+
+test('test apple or return',()=>{
+  console.log(applyOrReturn([1,2,3],(...args) => args))
+  console.log(applyOrReturn([1,2,3],'test'))
+})
