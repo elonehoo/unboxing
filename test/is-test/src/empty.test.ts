eac899562a344af59ec1f8af4fe67e55f3a993b8
xiaoxunyao <huchengyea@163.com>
2022-05-08 17:07:32 +0800

    chore: init `empty` test

---
+++ test/is-test/src/empty.test.ts
@@ -0,0 +1,11 @@
+import { isEmpty } from "@unboxing/is";
+import { test } from "vitest";
+test('test is empty',()=>{
+  console.log(isEmpty([]))
+  console.log(isEmpty(null))
+  console.log(isEmpty({}))
+  console.log(isEmpty(''))
+  console.log(isEmpty('test'))
+  console.log(isEmpty({ a: 5 }))
+  console.log(isEmpty([1]))
+})
