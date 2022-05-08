bf37e25deb501c16ee54343069c31aa6c689560b
xiaoxunyao <huchengyea@163.com>
2022-05-08 16:01:55 +0800

    chore: init `isObject`

--- packages/is/src/index.ts
+++ packages/is/src/index.ts
@@ -1,3 +1,4 @@
 export * from './array'
 export * from './boolean'
 export * from './number'
+export * from './object'
---
+++ packages/is/src/object.ts
@@ -0,0 +1,2 @@
+// Checks if `test` is object.
+export const isObject = (test:any): test is Record<any, any> => test !== null && typeof test === 'object'
