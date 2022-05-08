5fdd480b14b5826833c9cb0a86ff0ec2533671a8
xiaoxunyao <huchengyea@163.com>
2022-05-08 17:07:22 +0800

    chore: init `empty`

---
+++ packages/is/src/empty.ts
@@ -0,0 +1,20 @@
+import { isNil } from "./nil"
+import { isArray } from "./array"
+import { isString } from "./string"
+import { isObject } from "./object"
+
+// Checks if `test` is empty.
+export const isEmpty =  (test:any): boolean => {
+  switch (true) {
+      case isNil(test):
+          return true;
+      case isString(test):
+          return test === '';
+      case isArray(test):
+          return test.length === 0;
+      case isObject(test):
+          return Object.keys(test).length === 0;
+  }
+
+  return false;
+}
