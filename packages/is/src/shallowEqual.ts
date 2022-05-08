a99d18cbfbdf8c8bd9e873c5ccacf26f258a3d40
xiaoxunyao <huchengyea@163.com>
2022-05-08 17:42:13 +0800

    chore: init `shallowEqual`

---
+++ packages/is/src/shallowEqual.ts
@@ -0,0 +1,30 @@
+import { isObject } from "./object"
+
+
+const hasOwnProperty = Object.prototype.hasOwnProperty
+
+export const isShallowEqual = (test1:any, test2:any): boolean => {
+  if (test1 === test2) {
+      return true;
+  }
+
+  if (!isObject(test1) || !isObject(test2)) {
+      return false;
+  }
+
+  const keys = Object.keys(test1);
+
+  if (keys.length !== Object.keys(test2).length) {
+      return false;
+  }
+
+  for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+
+      if (!hasOwnProperty.call(test2, key) || test1[key] !== test2[key]) {
+          return false;
+      }
+  }
+
+  return true;
+}
