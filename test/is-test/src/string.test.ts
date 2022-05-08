0d230d20476ab723ad565520745189e16e7c8bc1
xiaoxunyao <huchengyea@163.com>
2022-05-08 16:17:24 +0800

    chore: init `string` test

---
+++ test/is-test/src/string.test.ts
@@ -0,0 +1,8 @@
+import { isString } from "@unboxing/is";
+import { test } from "vitest";
+
+test('test is string',()=>{
+  console.log(isString('abc'))
+  console.log(isString(null))
+  console.log(isString(new String('abc')))
+})
