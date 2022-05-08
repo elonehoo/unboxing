fa9fa9cf8cc9e883da311bab089171b218e18de4
xiaoxunyao <huchengyea@163.com>
2022-05-08 16:22:18 +0800

    chore: init `undefined` test

---
+++ test/is-test/src/undefined.test.ts
@@ -0,0 +1,7 @@
+import { isUndefined } from "@unboxing/is";
+import { test } from "vitest";
+
+test('test is undefined',()=>{
+  console.log(isUndefined(void 0))
+  console.log(isUndefined(null))
+})
