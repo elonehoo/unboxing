7ce18790d293db7b8a3e038f2d8ef8ee12128eeb
xiaoxunyao <huchengyea@163.com>
2022-05-08 12:06:51 +0800

    chore: init `complement` test

---
+++ test/function-test/src/complement.test.ts
@@ -0,0 +1,17 @@
+import { complement } from "@unboxing/function"
+import { test } from "vitest"
+
+test('test complement',()=>{
+  var isNotNil = complement(isNil)
+
+  console.log(isNil(null)) // true
+  console.log(isNotNil(null)) // false
+
+  console.log(isNil(7)) // false
+  console.log(isNotNil(7)) // true
+})
+
+function isNil(value:any){
+  return value === undefined || value === null
+}
+
