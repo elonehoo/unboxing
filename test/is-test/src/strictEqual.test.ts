b2814761b88aaebdf2e2ba6c92e9ca4801e3103a
xiaoxunyao <huchengyea@163.com>
2022-05-08 16:09:24 +0800

    chore: init `isStrictEqual` test

---
+++ test/is-test/src/strictEqual.test.ts
@@ -0,0 +1,9 @@
+import { isStrictEqual } from "@unboxing/is";
+import { test } from "vitest";
+test('test is strict equal',()=>{
+  console.log(isStrictEqual(1, 1))
+  console.log(isStrictEqual(1, '1'))
+  console.log(isStrictEqual({}, {}))
+  console.log(isStrictEqual(NaN, NaN))
+  console.log(isStrictEqual(-0, 0))
+})
