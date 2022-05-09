4fc16f3788a6b9625d896a4098a596819cdd98dd
xiaoxunyao <huchengyea@163.com>
2022-05-09 10:59:08 +0800

    chore: init `promise` model

---
+++ packages/promise/src/tap.ts
@@ -0,0 +1,14 @@
+import { curryN } from '@unboxing/function'
+import { Void1 } from '@unboxing/core'
+
+interface TapPromise {
+  <T>(fn: Void1<T>, x: T): Promise<T>;
+  (fn: Void1): <T>(x: T) => Promise<T>;
+}
+
+// Runs the given promise with the supplied argument. Returns the argument, when promise is resolved.
+export const promiseTap = curryN(2, <T>(fn: Void1<T>, x: T) =>
+    Promise.resolve()
+        .then(() => fn(x))
+        .then(() => x)
+) as TapPromise
