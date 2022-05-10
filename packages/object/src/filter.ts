18399eda7321326d18cade45a9bf7576b7896139
xiaoxunyao <huchengyea@163.com>
2022-05-10 13:31:02 +0800

    chore: init `filter`

---
+++ packages/object/src/filter.ts
@@ -0,0 +1,28 @@
+import { curryN } from "@unboxing/function";
+import {objectKeys} from './keys'
+import { ObjPred } from '@unboxing/core'
+
+interface FilterObj {
+  <O extends Record<any, any>>(fn: ObjPred<keyof O, O[keyof O]>, obj: O): Partial<O>;
+  <K extends string, V>(fn: ObjPred<K, V>): <O extends Record<K, V>>(obj: O) => Partial<O>;
+}
+
+/**
+ * Takes a predicate and a object, and returns a new object
+ * containing only members which satisfy the
+ * given predicate.
+ */
+export const filterObject = curryN(2, <O extends Record<any, any>>(fn: ObjPred<keyof O, O[keyof O]>, obj: O = {} as any) => {
+  const keys = objectKeys(obj);
+  const result: Partial<O> = {};
+
+  for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+
+      if (fn(obj[key], key, obj)) {
+          result[key] = obj[key];
+      }
+  }
+
+  return result;
+}) as FilterObj
