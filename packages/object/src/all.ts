097e923e73afb90111b439a1b2ff5a1bbb7ce425
xiaoxunyao <huchengyea@163.com>
2022-05-10 10:17:10 +0800

    fix: add `note`

--- packages/object/src/all.ts
+++ packages/object/src/all.ts
@@ -6,6 +6,7 @@ interface AllObject {
   <K extends string, V>(fn: ObjPred<K, V>): (obj: Record<K, V>) => boolean;
 }
 
+// Tests whether every [key, value] pair in the object passes the test implemented by the provided function.
 export const allObject:AllObject = curryN(2, <K extends string, V>(fn: ObjPred<K, V>, obj: Record<K, V> = {} as any) => {
   for (const key in obj) {
       if (Object.prototype.hasOwnProperty.call(obj, key) && !fn(obj[key], key, obj)) {
