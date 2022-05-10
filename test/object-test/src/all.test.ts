40cb793560bab85a1876642354d15c49c04faf3f
xiaoxunyao <huchengyea@163.com>
2022-05-10 09:54:24 +0800

    fix: init `test`

---
+++ test/object-test/src/all.test.ts
@@ -0,0 +1,13 @@
+import {test} from 'vitest'
+import { allObject } from '@unboxing/object'
+
+test('test all object',()=>{
+  var isBiggerThanZero = (x:number) => x > 0
+  console.log(allObject(isBiggerThanZero, {}))
+  console.log(allObject(isBiggerThanZero, { a: 1 }))
+  console.log(allObject(isBiggerThanZero, { a: 1, b: 1, c: 1 }))
+  console.log(allObject(isBiggerThanZero, { a: 0, b: 1, c: 0 }))
+  console.log(allObject(isBiggerThanZero, { a: 1, b: 0, c: 1 }))
+  console.log(allObject(isBiggerThanZero)({ a: 0, b: 0, c: 0 }))
+  console.log(allObject(isBiggerThanZero)({ a: 1, b: 0, c: 1 }))
+})
