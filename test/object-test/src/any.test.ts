31b3cc0f95dc40517c16b101206d88d34be8a687
xiaoxunyao <huchengyea@163.com>
2022-05-10 10:17:26 +0800

    chore: init `any` test

---
+++ test/object-test/src/any.test.ts
@@ -0,0 +1,12 @@
+import {test} from 'vitest'
+import { anyObject } from '@unboxing/object'
+
+test('test any object',()=>{
+  var isBiggerThanZero = (x:number) => x > 0
+  console.log(anyObject(isBiggerThanZero, {}))
+  console.log(anyObject(isBiggerThanZero, { a: 0 }))
+  console.log(anyObject(isBiggerThanZero, { a: 0, b: 1, c: 0 }))
+  console.log(anyObject(isBiggerThanZero, { a: 1, b: 0, c: -1 }))
+  console.log(anyObject(isBiggerThanZero)({ a: 0, b: 0, c: 0 }))
+  console.log(anyObject(isBiggerThanZero)({ a: 0, b: 1, c: 0 }))
+})
