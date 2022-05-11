baddb60572e4c466d00cd4ca64e834e54bb36078
xiaoxunyao <huchengyea@163.com>
2022-05-11 10:06:48 +0800

    chore: init `propApply`

---
+++ packages/object/src/propApply.ts
@@ -0,0 +1,16 @@
+import { curryN } from "@unboxing/function";
+import { prop } from "./prop";
+import { Prop, ObjBase } from '@unboxing/core'
+
+interface PropApply {
+  <K extends Prop, R, O extends Record<K, any>>(prop: K, fn: ObjBase<K, O[K], R>, obj: O): R;
+  <K extends Prop, V, R>(prop: K, fn: ObjBase<K, V, R>, obj:any): R;
+  <K extends Prop, V, R>(prop: K, fn: ObjBase<K, V, R>): (obj:any) => R;
+  <K extends Prop>(prop: K): {
+      <V, R>(fn: ObjBase<K, V, R>, obj:any): R;
+      <V, R>(fn: ObjBase<K, V, R>): (obj:any) => R;
+  };
+}
+
+// Returns the result of `fn` with value of property in `obj`.
+export const propApply = curryN(3, (propName: Prop, fn:any, obj:any) => fn(prop(propName, obj), propName, obj)) as PropApply
