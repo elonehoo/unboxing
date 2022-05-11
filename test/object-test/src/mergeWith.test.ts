e2cd9cec61948fdeac2dc0502efe6643f5b48a86
xiaoxunyao <huchengyea@163.com>
2022-05-11 08:58:57 +0800

    chore: init `mergeWith` test

---
+++ test/object-test/src/mergeWith.test.ts
@@ -0,0 +1,6 @@
+import { test } from "vitest";
+import { mergeWith } from '@unboxing/object'
+
+test('test mergeWith', () => {
+  console.log(mergeWith((x:any, y:any) => x + y, { 'name': 'fred', 'age': 10 }, { 'age': 40 }))
+})
