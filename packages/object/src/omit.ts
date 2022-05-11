57234eb3366227c217e7b27bf914e6b2248f1d5b
xiaoxunyao <huchengyea@163.com>
2022-05-11 09:06:43 +0800

    chore: init `omit`

---
+++ packages/object/src/omit.ts
@@ -0,0 +1,32 @@
+import { curryN } from "@unboxing/function"
+import { objectKeys } from "./keys"
+import { Prop } from '@unboxing/core'
+
+type Omitted<T, K extends Prop> = Pick<T, Exclude<keyof T, K>>
+
+interface Omit {
+    <K extends Prop, O>(props: ReadonlyArray<K>, obj: O): Omitted<O, K>;
+    <K extends Prop>(props: ReadonlyArray<K>): <O>(obj: O) => Omitted<O, K>;
+}
+
+// Returns a partial copy of an object omitting the keys specified.
+export const omit = curryN(2, <K extends Prop>(props: ReadonlyArray<K> = [], obj = {}) => {
+  const propsSet = Object.create(null);
+
+  for (let i = 0; i < props.length; i++) {
+      propsSet[props[i]] = true;
+  }
+
+  const result = {};
+  const keys = objectKeys(obj);
+
+  for (let i = 0; i < keys.length; i++) {
+      const prop = keys[i];
+
+      if (!propsSet[prop]) {
+          result[prop] = obj[prop];
+      }
+  }
+
+  return result;
+}) as Omit
