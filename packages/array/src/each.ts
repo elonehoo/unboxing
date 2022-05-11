ee6dba2fca35a5eaebb228e6019b3ace5daae4e1
xiaoxunyao <huchengyea@163.com>
2022-05-11 20:25:35 +0800

    chore: init `each`

---
+++ packages/array/src/each.ts
@@ -0,0 +1,14 @@
+import { curryN } from "@unboxing/function";
+import { ArrVoid } from '@unboxing/core'
+
+interface Each {
+  <T>(fn: ArrVoid<T>, arr: ArrayLike<T>): void;
+  <T>(fn: ArrVoid<T>): (arr: ArrayLike<T>) => void;
+}
+
+// Iterate over an input `list`, calling a provided function `fn` for each element in the list.
+export const eachString = curryN(2, <T>(fn: ArrVoid<T>, arr: ArrayLike<T> = []) => {
+  for (let i = 0; i < arr.length; i++) {
+      fn(arr[i], i, arr);
+  }
+}) as Each
