5a2c0dcc94a1704036a5e21d3ed409e13d0be9e9
xiaoxunyao <huchengyea@163.com>
2022-05-11 15:11:27 +0800

    chore: init `kebabCaseName`

---
+++ packages/string/src/kebabCaseName.ts
@@ -0,0 +1,8 @@
+import { words } from "./words";
+
+// Converts string to kebab case.
+export function kebabCaseName(str: string): string {
+  return words(str)
+      .map((word) => word.toLowerCase())
+      .join('-');
+}
