d1ca72b356b07bdda94563cfe3ec47b8566be706
xiaoxunyao <huchengyea@163.com>
2022-05-11 15:08:54 +0800

    chore: init `repeat`

---
+++ packages/string/src/repeat.ts
@@ -0,0 +1,22 @@
+import { curryN } from "@unboxing/function";
+import { isString } from '@unboxing/is'
+
+interface Repeat {
+  (n: number, a: string): string;
+  (n: number): (a: string) => string;
+}
+
+// Returns a string containing a repeated identical value.
+export const repeat = curryN(2, (n = 0, value: string) => {
+  if (!isString(value)) {
+      return '';
+  }
+
+  let result = '';
+
+  for (let i = 0; i < n; i++) {
+      result += value;
+  }
+
+  return result;
+}) as Repeat
