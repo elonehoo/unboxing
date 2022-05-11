1acf6b8fbbe4a617ed22005a2a86d3e12827a67c
xiaoxunyao <huchengyea@163.com>
2022-05-11 11:00:54 +0800

    chore: init `where`

---
+++ packages/object/src/where.ts
@@ -0,0 +1,26 @@
+import { curryN } from "@unboxing/function";
+import { has } from "./has";
+import { ObjPredBy, ObjPred, Prop } from '@unboxing/core'
+
+interface Where {
+  <O>(spec: Partial<Record<keyof O, ObjPredBy<O>>>, obj: O): boolean;
+  (spec: Record<Prop, ObjPred<string, any>>): (obj:any) => boolean;
+}
+
+/**
+ * Takes a spec object and a test object; returns true if the test satisfies
+ * the spec. Each of the spec's own properties must be a predicate function.
+ * Each predicate is applied to the value of the corresponding property of the
+ * test object. `where` returns true if all the predicates return true, false
+ * otherwise.
+ */
+export const where = curryN(2, <O>(spec: Partial<Record<keyof O, ObjPredBy<O>>> = {}, obj: O = {} as any) => {
+  for (const prop in spec) {
+      if (has(prop, spec) && !spec[prop](obj[prop], prop, obj)) {
+          return false;
+      }
+  }
+
+  return true;
+}) as Where
+
