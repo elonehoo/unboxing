21048700cb815bd7ed9772dfc5cd806df53e3ae1
xiaoxunyao <huchengyea@163.com>
2022-05-11 09:55:56 +0800

    chore: init `pickBy`

---
+++ packages/object/src/pickBy.ts
@@ -0,0 +1,20 @@
+import { curryN } from "@unboxing/function";
+import { ObjPred, ObjPredBy } from '@unboxing/core'
+
+interface PickBy {
+  <O>(pred: ObjPredBy<O>, obj: O): Partial<O>;
+  <K extends string>(pred: ObjPred<K, any>): <O>(obj: O) => Partial<O>;
+}
+
+// Returns a partial copy of an object containing only the keys with `fn` predicate returns true
+export const pickBy = curryN(2, <O>(fn: ObjPredBy<O>, obj: O = {} as any) => {
+  const result: Partial<O> = {};
+
+  for (const prop in obj) {
+      if (fn(obj[prop], prop, obj)) {
+          result[prop] = obj[prop];
+      }
+  }
+
+  return result;
+}) as PickBy
