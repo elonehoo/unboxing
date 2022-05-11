b4ccf8e07a3cc2c87f65b9d7d5df2a1b2b01cbfb
xiaoxunyao <huchengyea@163.com>
2022-05-11 10:54:46 +0800

    chore: init `values`

---
+++ packages/object/src/values.ts
@@ -0,0 +1,17 @@
+import { objectKeys } from "./keys";
+
+interface Values {
+  <O>(obj: O): Array<O[keyof O]>;
+}
+
+export const values = ((obj) => {
+  const keys = objectKeys(obj);
+  const len = keys.length;
+  const values = new Array(len);
+
+  for (let i = 0; i < len; i++) {
+      values[i] = obj[keys[i]];
+  }
+
+  return values;
+}) as Values
