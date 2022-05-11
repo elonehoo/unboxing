fbca970c0e7b02fd2f44eb53aaf40ff5025e997f
xiaoxunyao <huchengyea@163.com>
2022-05-11 10:06:55 +0800

    chore: init `propApply` test

---
+++ test/object-test/src/propApply.test.ts
@@ -0,0 +1,7 @@
+import { propApply } from "@unboxing/object";
+import { test } from "vitest";
+
+test('test prop apple',()=>{
+  console.log(propApply('a', x => 'is ' + x, {a: 2}))
+  console.log(propApply('b', x => x > 0, {b: 2}))
+})
