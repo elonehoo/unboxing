c4c701af470e39d4f3c3d98658960de61641f8ea
xiaoxunyao <huchengyea@163.com>
2022-05-11 10:45:33 +0800

    chore: init `toPairs`

---
+++ packages/object/src/toPairs.ts
@@ -0,0 +1,24 @@
+import { objectKeys } from "./keys";
+import { Prop, KeyValuePairs } from '@unboxing/core'
+
+interface ToPairs {
+  <K extends Prop, V>(obj: Record<K, V>): KeyValuePairs<K & string, V>;
+}
+
+/**
+ * Converts an object into an array of key, value arrays. Only the object's
+ * own properties are used.
+ * Note that the order of the output array is not guaranteed to be consistent
+ * across different JS platforms.
+ */
+export const toPairs = (<K extends Prop, V>(obj: Record<K, V> = {} as any) => {
+  const keys = objectKeys(obj);
+  const len = keys.length;
+  const result = new Array(len);
+
+  for (let i = 0; i < len; i++) {
+      result[i] = [keys[i], obj[keys[i]]];
+  }
+
+  return result;
+}) as ToPairs
