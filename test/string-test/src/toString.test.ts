b08ecb17d2e78261480489bb5456d76792b61574
xiaoxunyao <huchengyea@163.com>
2022-05-11 14:35:00 +0800

    chore: init `toString` test

---
+++ test/string-test/src/toString.test.ts
@@ -0,0 +1,7 @@
+import { test } from "vitest";
+import {toString} from '@unboxing/string'
+test('test to string',()=>{
+  console.log(toString(null))
+  console.log(toString('test'))
+  console.log(toString([1,2,3]))
+})
