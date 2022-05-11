30b61459b0186c2c30ea3e0ba96a2def96a8195b
xiaoxunyao <huchengyea@163.com>
2022-05-11 14:23:09 +0800

    chore: init `trim`

--- packages/string/src/index.ts
+++ packages/string/src/index.ts
@@ -2,3 +2,6 @@ export * from './words'
 export * from './upperFirst'
 export * from './unescape'
 export * from './trimLeft'
+export * from './trim'
+
+
---
+++ packages/string/src/trim.ts
@@ -0,0 +1,2 @@
+// Removes leading and trailing whitespace from `str`.
+export const trim = (str = '') => str.toString().trim() || ''
