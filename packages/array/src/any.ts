9a7962d6da0b8a221fcb2ec392c038ad79d9021a
xiaoxunyao <huchengyea@163.com>
2022-05-11 20:18:39 +0800

    chore: init `any`

---
+++ packages/array/src/any.ts
@@ -0,0 +1,17 @@
+import { curryN } from "@unboxing/function";
+import { ArrPred } from '@unboxing/core'
+
+interface Any {
+  <T>(fn: ArrPred<T>, list: ArrayLike<T>): boolean;
+  <T>(fn: ArrPred<T>): (list: ArrayLike<T>) => boolean;
+}
+
+// Returns `true` if at least one of elements of the list match the predicate, `false` otherwise.
+export const anyString = curryN(2, <T>(fn: ArrPred<T>, arr: ArrayLike<T> = []) => {
+  for (let i = 0; i < arr.length; i++) {
+      if (fn(arr[i], i, arr)) {
+          return true;
+      }
+  }
+  return false;
+}) as Any
