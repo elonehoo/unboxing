cbc62a17448992e6bdef2a8b207136be5369ddb1
xiaoxunyao <huchengyea@163.com>
2022-05-11 10:21:37 +0800

    chore: init `prop set`

---
+++ packages/object/src/propSet.ts
@@ -0,0 +1,23 @@
+import { curryN } from "@unboxing/function";
+import { Prop, ReplaceType } from '@unboxing/core'
+import { has } from "./has";
+
+interface PropSet {
+  <K extends Prop, V, O>(prop: K, val: V, obj: O): ReplaceType<O, K, V>;
+  <K extends Prop, V>(prop: K, val: V): <O>(obj: O) => ReplaceType<O, K, V>;
+  <K extends Prop>(prop: K): {
+      <V, O>(val: V, obj: O): ReplaceType<O, K, V>;
+      <V>(val: V): <O>(obj: O) => ReplaceType<O, K, V>;
+  };
+}
+
+export const propSet = curryN(3, <K extends Prop>(prop: K, val:any, obj = {} as any) => {
+  if (has(prop, obj) && obj[prop] === val) {
+      return obj;
+  }
+
+  return {
+      ...obj,
+      [prop]: val,
+  };
+}) as PropSet
