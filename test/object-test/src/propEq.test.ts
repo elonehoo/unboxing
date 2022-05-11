1afc7618e0ebbe61a0ee1c1811618e48502ef369
xiaoxunyao <huchengyea@163.com>
2022-05-11 10:13:28 +0800

    chore: init `propEq` test

---
+++ test/object-test/src/propEq.test.ts
@@ -0,0 +1,13 @@
+import { filterObject, propEq } from "@unboxing/object";
+
+import { test } from "vitest";
+
+test('test prop eq',()=>{
+  var abby = {name: 'Abby', age: 7, hair: 'blond'};
+     var fred = {name: 'Fred', age: 12, hair: 'brown'};
+     var rusty = {name: 'Rusty', age: 10, hair: 'brown'};
+     var alois = {name: 'Alois', age: 15, disposition: 'surly'};
+     var kids = [abby, fred, rusty, alois];
+     var hasBrownHair = propEq('hair', 'brown')
+  console.log(filterObject(hasBrownHair, kids))
+})
