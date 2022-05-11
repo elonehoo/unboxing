05ac130ef893f4b021db15950000e593c931c7d9
xiaoxunyao <huchengyea@163.com>
2022-05-11 15:02:40 +0800

    chore: init `replace` test

---
+++ test/string-test/src/replace.test.ts
@@ -0,0 +1,8 @@
+import replace from "@unboxing/string/src/replace";
+import { test } from "vitest";
+
+test('test replace',()=>{
+  console.log(replace('foo', 'bar', 'foo foo foo'))
+  console.log(replace(/foo/, 'bar', 'foo foo foo'))
+  console.log(replace(/foo/g, 'bar', 'foo foo foo'))
+})
