a4e57375fd46d3d8e79816af10e514ba432f2afd
xiaoxunyao <huchengyea@163.com>
2022-05-12 11:04:32 +0800

    chore: init `take` test

---
+++ test/array-test/src/take.test.ts
@@ -0,0 +1,10 @@
+import { takeArray } from "@unboxing/array";
+import { test } from "vitest";
+
+test('test take array',()=>{
+  console.log(takeArray(1, ['foo', 'bar', 'baz']))
+  console.log(takeArray(2, ['foo', 'bar', 'baz']))
+  console.log(takeArray(3, ['foo', 'bar', 'baz']))
+  console.log(takeArray(4, ['foo', 'bar', 'baz']))
+  console.log(takeArray(3, 'ramda'))
+})
