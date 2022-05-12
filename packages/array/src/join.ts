6d28410f36f162e24c11456d825b9c3010950846
xiaoxunyao <huchengyea@163.com>
2022-05-12 09:18:00 +0800

    chore: init `join`

---
+++ packages/array/src/join.ts
@@ -0,0 +1,23 @@
+import { curryN } from "@unboxing/function";
+import { isArray } from "@unboxing/is";
+
+interface Join {
+  (x: string, xs: ArrayLike<any>): string;
+  (x: string): (xs: ArrayLike<any>) => string;
+}
+
+/**
+ * Returns a string made by inserting the `separator` between each element and
+ * concatenating all the elements into a single string.
+ */
+export const joinArray = curryN(2, <T>(separator = '', arr: ArrayLike<T> = []) => {
+  let resultArr: T[];
+
+  if (isArray(arr)) {
+      resultArr = arr;
+  } else {
+      resultArr = Array.prototype.slice.call(arr);
+  }
+
+  return resultArr.join(separator);
+}) as Join
