f9ec95b0d8797ffa3a17c081ab01e65b9c390517
xiaoxunyao <huchengyea@163.com>
2022-05-12 10:40:32 +0800

    chore: init `sort` test

---
+++ test/array-test/src/sort.test.ts
@@ -0,0 +1,8 @@
+import { sortArray } from "@unboxing/array";
+import { test } from "vitest";
+
+test('test sort array',()=>{
+  var diff = function(a:number, b:number) { return a - b; }
+  console.log(sortArray(diff, [4,2,7,5]))
+
+})
