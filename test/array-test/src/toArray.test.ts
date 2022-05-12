f7d2c681464aecb386cd0f69b0ed2b588fcfb33a
xiaoxunyao <huchengyea@163.com>
2022-05-12 11:11:17 +0800

    chore: init `toArray` test

---
+++ test/array-test/src/toArray.test.ts
@@ -0,0 +1,7 @@
+import { toArray } from "@unboxing/array";
+import { test } from "vitest";
+test('test to array',()=>{
+  console.log(toArray([1,2,3]))
+  console.log(toArray(5))
+  console.log(toArray('test'))
+})
