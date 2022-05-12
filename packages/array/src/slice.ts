a57564a903352ff45f0dc7128fd923a0307ceb0b
xiaoxunyao <huchengyea@163.com>
2022-05-12 10:35:57 +0800

    chore: init `slice`

---
+++ packages/array/src/slice.ts
@@ -0,0 +1,27 @@
+import { curryN } from "@unboxing/function";
+import { isString } from "@unboxing/is";
+
+interface Slice {
+  (a: number, b: number, list: string): string;
+  <T>(a: number, b: number, list: ArrayLike<T>): T[];
+  (a: number, b: number): {
+      (list: string): string;
+      <T>(list: ArrayLike<T>): T[];
+  };
+  (a: number): {
+      (b: number, list: string): string;
+      <T>(b: number, list: ArrayLike<T>): T[];
+      (b: number): {
+          (list: string): string;
+          <T>(list: ArrayLike<T>): T[];
+      };
+  };
+}
+
+/**
+ * Returns the elements of the given list or string (or object with a `slice`
+ * method) from `fromIndex` (inclusive) to `toIndex` (exclusive).
+ */
+export const sliceArray = curryN(3, <T>(fromIndex: number, toIndex: number, list: ArrayLike<T> | string = []) =>
+    isString(list) ? list.slice(fromIndex, toIndex) : Array.prototype.slice.call(list, fromIndex, toIndex)
+) as Slice
