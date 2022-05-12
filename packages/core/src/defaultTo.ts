68dc9b205cd3134bf4d4046b70a7221f865be1a0
xiaoxunyao <huchengyea@163.com>
2022-05-12 14:46:41 +0800

    chore: init `defaultTo`

---
+++ packages/core/src/defaultTo.ts
@@ -0,0 +1,23 @@
+import { curryN } from "@unboxing/function";
+
+interface DefaultTo {
+  <T>(a: T, b: null | undefined): T;
+  <T, U>(a: T, b: U): U;
+  <T>(a: T): {
+      (b: null | undefined): T;
+      <U>(b: U): U;
+  };
+}
+
+/**
+ * Returns the second argument if it is not `null`, `undefined` or `NaN`
+ * otherwise the first argument is returned.
+ */
+export const defaultTo = curryN(2, (dflt:any, x:any) => {
+  if (x == null || x !== x) {
+      // eslint-disable-line no-self-compare
+      return dflt;
+  }
+
+  return x;
+}) as DefaultTo
