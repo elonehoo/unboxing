0e00203031e9d2be3dd87d4d0b17a15896b96c5a
xiaoxunyao <huchengyea@163.com>
2022-05-12 09:23:12 +0800

    chore: init `length` test

---
+++ test/array-test/src/length.test.ts
@@ -0,0 +1,6 @@
+import { lengthArray } from "@unboxing/array";
+import { test } from "vitest";
+test('test length array',()=>{
+  console.log(lengthArray([]))
+  console.log(lengthArray([1,2,3,4]))
+})
