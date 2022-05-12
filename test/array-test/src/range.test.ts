92baad7b52c34545e060d186c5797542c1576bf2
xiaoxunyao <huchengyea@163.com>
2022-05-12 09:55:35 +0800

    chore: init `range` test

---
+++ test/array-test/src/range.test.ts
@@ -0,0 +1,11 @@
+import { range } from "@unboxing/array";
+import { test } from "vitest";
+
+test('test range',()=>{
+  console.log(range(4))
+  console.log(range(-4))
+  console.log(range(1, 5))
+  console.log(range(0, 20, 5))
+  console.log(range(0, -4, -1))
+  console.log(range(0))
+})
