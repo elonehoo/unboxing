28be7492f38c4265ca2f037f1bd2732da899913e
xiaoxunyao <huchengyea@163.com>
2022-05-12 13:36:25 +0800

    chore: init `update`

---
+++ packages/array/src/update.ts
@@ -0,0 +1,13 @@
+import { curryN,always } from "@unboxing/function";
+import { adjustArray } from "./adjust";
+
+interface Update {
+  <T>(index: number, value: T, list: ArrayLike<T>): T[];
+  <T>(index: number, value: T): (list: ArrayLike<T>) => T[];
+  <T>(index: number): {
+      <T>(value: T, list: ArrayLike<T>): T[];
+      <T>(value: T): (list: ArrayLike<T>) => T[];
+  };
+}
+
+export const updateArray = curryN(3, <T>(idx: number, x: T, list: ArrayLike<T>) => adjustArray(always(x), idx, list)) as Update
