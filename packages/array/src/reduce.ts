4b9479f750370d95a5bdf9077dbff742399dcd5f
xiaoxunyao <huchengyea@163.com>
2022-05-12 09:59:55 +0800

    chore: init `reduce`

---
+++ packages/array/src/reduce.ts
@@ -0,0 +1,25 @@
+import { curryN } from "@unboxing/function";
+import { CurriedFunction2 } from '@unboxing/core'
+
+export type ReduceFunc<T, R> = (acc: R, elem: T, index: number, arr: ArrayLike<T>) => R;
+
+interface Reduce {
+    <T, R>(fn: ReduceFunc<T, R>, acc: R, list: ArrayLike<T>): R;
+    <T, R>(fn: ReduceFunc<T, R>, acc: R): (list: ArrayLike<T>) => R;
+    <T, R>(fn: ReduceFunc<T, R>): CurriedFunction2<R, ArrayLike<T>, R>;
+}
+
+/**
+ * Returns a single item by iterating through the list, successively calling
+ * the iterator function and passing it an accumulator value and the current
+ * value from the array, and then passing the result to the next call.
+ */
+export const reduceArray = curryN(3, <T, R>(fn: ReduceFunc<T, R>, acc: R, arr: ArrayLike<T> = []) => {
+  const len = arr.length;
+
+  for (let i = 0; i < len; i++) {
+      acc = fn(acc, arr[i], i, arr);
+  }
+
+  return acc;
+}) as Reduce
