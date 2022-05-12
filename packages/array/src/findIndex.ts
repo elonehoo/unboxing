dc318258dd2f75993bccaadce8d396b406783c9a
xiaoxunyao <huchengyea@163.com>
2022-05-12 08:46:30 +0800

    chore: init `findIndex`

---
+++ packages/array/src/findIndex.ts
@@ -0,0 +1,18 @@
+import { curryN } from "@unboxing/function"
+import { ArrPred } from '@unboxing/core'
+
+interface FindIndex {
+  <T>(fn: ArrPred<T>, list: ArrayLike<T>): number;
+  <T>(fn: ArrPred<T>): (list: ArrayLike<T>) => number;
+}
+
+// Returns the index of the first element of the list which matches the predicate, or `-1` if no element matches.
+export const findStringIndex = curryN(2, <T>(fn: ArrPred<T>, arr: ArrayLike<T> = []) => {
+  for (let i = 0; i < arr.length; i++) {
+      if (fn(arr[i], i, arr)) {
+          return i;
+      }
+  }
+
+  return -1;
+}) as FindIndex
