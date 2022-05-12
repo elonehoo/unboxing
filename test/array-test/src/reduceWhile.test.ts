cd20028c5faed75860830187960b06ab12f3ad58
xiaoxunyao <huchengyea@163.com>
2022-05-12 10:08:15 +0800

    chore: init `reduceWhile` test

---
+++ test/array-test/src/reduceWhile.test.ts
@@ -0,0 +1,6 @@
+import { reduceWhile } from "@unboxing/array";
+import { test } from "vitest";
+
+test('test reduce while',()=>{
+  console.log(reduceWhile(acc => acc.length < 3, (acc, x) => acc + x, '1', ['2', '3', '4', '5']))
+})
