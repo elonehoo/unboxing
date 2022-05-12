dd5fc34cbfa7aa606ec2dfcd13bf39251558a5d0
xiaoxunyao <huchengyea@163.com>
2022-05-12 13:50:26 +0800

    chore: init `drop` test

---
+++ test/array-test/src/drop.test.ts
@@ -0,0 +1,9 @@
+import { dropArray } from "@unboxing/array";
+import { test } from "vitest";
+
+test('test drop array',()=>{
+  console.log(dropArray(1, ['foo', 'bar', 'baz']))
+  console.log(dropArray(2, ['foo', 'bar', 'baz']))
+  console.log(dropArray(3, ['foo', 'bar', 'baz']))
+  console.log(dropArray(4, ['foo', 'bar', 'baz']))
+})
