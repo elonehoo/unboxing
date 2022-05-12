719541b767b73621a627473954ea2ded27af551c
xiaoxunyao <huchengyea@163.com>
2022-05-12 09:20:58 +0800

    chore: init `last`

---
+++ packages/array/src/last.ts
@@ -0,0 +1,9 @@
+interface Last {
+  <T>(list: ArrayLike<T>): T | undefined;
+  (list: string): string;
+}
+
+/**
+ * Returns the last element of the given array.
+ */
+export const last = (<T>(arr: ArrayLike<T> | string) => arr && arr[arr.length - 1]) as Last
