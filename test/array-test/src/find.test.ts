1f11113631603c7e39839112ebce8fe740f7358e
xiaoxunyao <huchengyea@163.com>
2022-05-12 08:45:05 +0800

    fix: update `find` test

--- test/array-test/src/find.test.ts
+++ test/array-test/src/find.test.ts
@@ -3,6 +3,6 @@ import { test } from "vitest";
 
 test('test find string',()=>{
   var xs = [{a: 1}, {a: 2}, {a: 3}]
-  console.log(findString(x => x.a === 2)(xs))
-  console.log(findString(x => x.a === 4)(xs))
+  console.log(findString((x:any) => x.a === 2)(xs))
+  console.log(findString((x:any) => x.a === 4)(xs))
 })
