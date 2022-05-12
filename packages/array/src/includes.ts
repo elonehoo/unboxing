c398ca5b5a9a8ccebcd99a83e3f2396653333e5c
xiaoxunyao <huchengyea@163.com>
2022-05-12 09:08:56 +0800

    chore: init `includes`

---
+++ packages/array/src/includes.ts
@@ -0,0 +1,14 @@
+import { curryN } from "@unboxing/function";
+import { indexOf } from "./indexOf";
+
+interface Includes {
+  (value: string, array: string): boolean;
+  <T>(value: T, array: ArrayLike<T>): boolean;
+  <T>(value: T): (array: ArrayLike<T> | string) => boolean;
+}
+
+/**
+ * Dispatches call to arr.indexOf, returns true if arr is array and value in the array or
+ * if arr is string and value is substring of arr
+ */
+export const includes = curryN(2, <T>(value: T, arr: ArrayLike<T> = []) => indexOf(value, arr) !== -1) as Includes
