6681799df2aef1578b1e0c6a69c48766f634f80f
xiaoxunyao <huchengyea@163.com>
2022-05-12 13:25:56 +0800

    chore: init `concat` test

---
+++ test/array-test/src/concat.test.ts
@@ -0,0 +1,8 @@
+import { concat } from "@unboxing/array";
+import { test } from "vitest";
+
+test('test concat',()=>{
+  console.log(concat('abc','cde'))
+  console.log(concat([4, 5, 6], [1, 2, 3]))
+  console.log(concat([], []))
+})
