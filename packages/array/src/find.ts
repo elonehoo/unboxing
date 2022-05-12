5eff3fbe25c17e34057380a7789f283f3cd9844c
xiaoxunyao <huchengyea@163.com>
2022-05-12 08:42:14 +0800

    chore: init `find`

---
+++ packages/array/src/find.ts
@@ -0,0 +1,16 @@
+import { curryN } from "@unboxing/function";
+import { ArrPred } from '@unboxing/core'
+
+interface Find {
+  <T>(fn: ArrPred<T>, list: ArrayLike<T>): T | undefined;
+  <T>(fn: ArrPred<T>): (list: ArrayLike<T>) => T | undefined;
+}
+
+// Returns the first element of the list which matches the predicate, or `undefined` if no element matches.
+export const findString = curryN(2, <T>(fn: ArrPred<T>, arr: ArrayLike<T> = []) => {
+  for (let i = 0; i < arr.length; i++) {
+      if (fn(arr[i], i, arr)) {
+          return arr[i];
+      }
+  }
+}) as Find
