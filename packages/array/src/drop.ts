9e32a5fe62ca83fedcc09cdc58be7bcd7ff61931
xiaoxunyao <huchengyea@163.com>
2022-05-12 13:50:18 +0800

    chore: init `drop`

---
+++ packages/array/src/drop.ts
@@ -0,0 +1,16 @@
+import { curryN } from "@unboxing/function";
+import { sliceArray } from "./slice";
+
+interface Drop {
+  (n: number, xs: string): string;
+  <T>(n: number, xs: ArrayLike<T>): T[];
+  (n: number): {
+      (xs: string): string;
+      <T>(xs: ArrayLike<T>): T[];
+  };
+}
+
+/**
+ * Returns all but the first `n` elements of the given list, string.
+ */
+export const dropArray = curryN(2, <T>(n: number, xs: ArrayLike<T>) => sliceArray(Math.max(0, n), Infinity, xs)) as Drop
