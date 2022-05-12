dafe56b31f9a266403f3397623046aa5aa39936b
xiaoxunyao <huchengyea@163.com>
2022-05-12 13:39:55 +0800

    chore: init `difference`

---
+++ packages/array/src/difference.ts
@@ -0,0 +1,23 @@
+import { curryN } from "@unboxing/function";
+import { indexOf } from "./indexOf";
+
+interface Difference {
+  <T>(list1: ArrayLike<T>, list2: ArrayLike<T>): T[];
+  <T>(list1: ArrayLike<T>): (list2: ArrayLike<T>) => T[];
+}
+
+/**
+ * Returns the array of all elements in the first array not
+ * contained in the second array.
+ */
+export const difference = curryN(2, <T>(a: ArrayLike<T> = [], b: ArrayLike<T> = []) => {
+  const result = [];
+
+  for (let i = 0; i < a.length; i++) {
+      if (indexOf(a[i], b) < 0) {
+          result.push(a[i]);
+      }
+  }
+
+  return result;
+}) as Difference
