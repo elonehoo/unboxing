85d45bcaed2c08b6130bd8d71cce89544ac5d15b
xiaoxunyao <huchengyea@163.com>
2022-05-12 08:42:22 +0800

    chore: init `find` test

---
+++ test/array-test/src/find.test.ts
@@ -0,0 +1,8 @@
+import { findString } from "@unboxing/array";
+import { test } from "vitest";
+
+test('test find string',()=>{
+  var xs = [{a: 1}, {a: 2}, {a: 3}]
+  console.log(findString(x => x.a === 2)(xs))
+  console.log(findString(x => x.a === 4)(xs))
+})
