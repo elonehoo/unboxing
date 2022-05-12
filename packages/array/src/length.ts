30109dd72476bd87acc194728aca6bac2b2a9ada
xiaoxunyao <huchengyea@163.com>
2022-05-12 09:23:05 +0800

    chore: init `length`

---
+++ packages/array/src/length.ts
@@ -0,0 +1,8 @@
+interface Length {
+  <T>(list: T): T extends ArrayLike<any> ? number : void;
+}
+
+/**
+ * Returns the number of elements in the array by returning `arr.length`.
+ */
+export const lengthArray = ((arr?: ArrayLike<any>) => arr && arr.length) as Length
